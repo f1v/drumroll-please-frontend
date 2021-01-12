880d1e754d1fb442291894196d92fa57ba9ed241
ABraun <abraun@cainc.com>
2021-01-12 15:30:31 -0500

    feat(beatindicator): Add indicator for beat position

---
+++ src/components/BeatIndicator.tsx
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react';
+import classnames from 'classnames';
+import {
+  Container,
+  Grid,
+  makeStyles,
+  Paper,
+  Theme,
+} from '@material-ui/core';
+import { SchedulerContext } from '../context/SchedulerContext';
+
+function BeatIndicator() {
+  // @ts-ignore Type '{ state: SchedulerContextType; dispatch: Dispatch<any>; }' is not an array type.
+  const [state, dispatch] = useContext(SchedulerContext);
+  const useStyles = makeStyles((theme: Theme) => ({
+    container: {
+      position: 'relative',
+      padding: '20px 0',
+    },
+    card: {
+      width: '6.25%',
+      position: 'relative',
+    },
+    indicator: {
+      backgroundColor: '#fff',
+      height: 0,
+      paddingBottom: '25%',
+      width: '100%',
+    },
+    active: {
+      backgroundColor: '#b5d3e7',
+    }
+  }));
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.container}>
+      <Grid container spacing={1}>
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((value) => (
+          <Grid key={value} item className={classes.card}>
+            <Paper className={classnames(classes.indicator, value === state.beat && classes.active)}/>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  )
+}
+
+export default BeatIndicator;
