6a4b6b5ed7b50bd7706c8392e3adc3276608b8e0
ABraun <abraun@cainc.com>
2021-01-12 14:20:53 -0500

    feat(Rebase for modules): Rebase to work with modules

--- src/components/Sequencer.tsx
+++ src/components/Sequencer.tsx
@@ -1,4 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, {
+  useContext,
+  useEffect,
+  useRef,
+  useState
+} from 'react';
+import audioCtx from '../utils/audioContext';
+import { notesInQueue } from '../utils/scheduler';
 import {
   Container,
   makeStyles,
@@ -8,6 +15,9 @@ import Icon from '@material-ui/core/Icon';
 import Module from '../components/Module'
 import { getLoopSequence } from '../api/loop'
 import { updateModule } from '../api/module';
+import PlayButton from './PlayButton';
+import TempoSlider from './TempoSlider';
+import { SchedulerContext } from '../context/SchedulerContext';
 
 declare type Props = {
   match: {
@@ -23,12 +33,52 @@ const useStyles = makeStyles((theme: Theme) => ({}));
 
 function Sequencer({ match }: Props) {
   const classes = useStyles();
+  const requestRef = useRef<number>(0);
+  // @ts-ignore Type '{ state: SchedulerContextType; dispatch: Dispatch<any>; }' is not an array type.
+  const [state, dispatch] = useContext(SchedulerContext);
+
   const [sequenceData, setSequenceData] = useState({
     name: '',
     id: null,
     modules: [],
   })
   const [shouldRefreshData, setShouldRefreshData] = useState(false)
+
+  /**
+   * requestAnimationFrame keeps track of beat count from the scheduler.
+   * Does some housekeeping and dispatches to BEAT to update the beat in the provider store.
+   */
+  let lastNoteDrawn = 3;
+  const beatProgressAnimation = () => {
+    let drawNote = lastNoteDrawn;
+    const currentTime = audioCtx.currentTime;
+    while (notesInQueue.length && notesInQueue[0].time < currentTime) {
+      drawNote = notesInQueue[0].note;
+      notesInQueue.splice(0, 1);   // remove note from queue
+      dispatch({
+        type: 'BEAT',
+        payload: drawNote,
+      })
+    }
+    // We only need to draw if the note has moved.
+    if (lastNoteDrawn !== drawNote) {
+      lastNoteDrawn = drawNote;
+    }
+    // set up to draw again
+    requestRef.current = requestAnimationFrame(beatProgressAnimation);
+  }
+
+  /**
+   * Start/stop state tells us when to start animating
+   */
+  useEffect(() => {
+    if (state.isPlaying) {
+      requestAnimationFrame(beatProgressAnimation);
+    } else {
+      cancelAnimationFrame(requestRef.current);
+    }
+  }, [state.isPlaying]);
+
 
   useEffect(() => {
     console.log('use effect')
@@ -59,6 +109,9 @@ function Sequencer({ match }: Props) {
 
   return (
     <Container maxWidth="lg">
+      <div>Current Beat: {state.beat}</div>
+      <PlayButton />
+      <TempoSlider />
       Sequencer
       name: {sequenceData.name}
       {modules && modules.map(createModules)}
