621f1439c4cddf68543c05d45406f629441e7733
Alex Tsuji <f1v-9@F1V-9s-MacBook-Pro.local>
2021-01-12 15:35:13 -0500

    WIP style instrument display

--- src/components/InstrumentDisplay.tsx
+++ src/components/InstrumentDisplay.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import {
+  Container,
+  makeStyles,
+  Theme,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  instrumentContainer: {
+    border: 'solid black 2px',
+    height: '100px',
+    width: '100px'
+  }
+}));
 
 declare type Props = {
   instrument: string
 }
 function InstrumentDisplay({ instrument }: Props) {
-
+  const classes = useStyles()
   return (
-    <div>
+    <Container className={classes.instrumentContainer}>
       Instrument: {instrument}
-    </div>
+    </Container>
   )
 }
 
